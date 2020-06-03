a14ba7f6d1f0937d2d7c4079475ed64912446df7
Dorota Pindur <dpindur@wp.pl>
2020-06-03 09:02:29 +0200

    practice page added

--- webpack.common.js
+++ webpack.common.js
@@ -63,5 +63,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/practice.html",
+      inject: true,
+      chunks: ["index", "practice"],
+      filename: "practice.html",
+    }),
   ],
 };
